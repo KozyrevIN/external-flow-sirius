fdc65131cfe41ed34d3c720bf86278b017d48587
Hello <coollemon1000@gmail.com>
2025-08-06 15:44:43 +0300

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 #include <iostream>
 #include <string>
 
@@ -26,41 +25,3 @@ int main(int argc, char *argv[]) {
 
     return EXIT_SUCCESS;
 }
-=======
-#include <iostream>
-#include <string>
-
-#include <vtkPolyData.h>
-#include <vtkSmartPointer.h>
-
-#include "../include/utils.h"
-#include "../include/kernels.h"
-#include "../include/geometry.h"
-const std::string mesh_path = "meshes/sphere_642.vtk";
-#include <vtkPoints.h>
-
-// Функция для печати координат точки по её индексу
-void print_point(vtkPolyData* polyData, vtkIdType pointId) {
-    vtkPoints* points = polyData->GetPoints();
-    
-    double point[3];
-    points->GetPoint(pointId, point);
-    std::cout << "Точка " << pointId << ": (" 
-              << point[0] << ", " 
-              << point[1] << ", " 
-              << point[2] << ")" << std::endl;
-}
-
-int main(int argc, char *argv[]) {
-    vtkSmartPointer<vtkPolyData> polyData = load_mesh(mesh_path,true);
-    print_point(polyData, 10);
-    
-    // Добавляем центры ячеек к сетке
-    add_center_to_cells(polyData);
-    
-    // Сохраняем сетку с центрами ячеек
-    save_mesh(polyData, "output_with_centers.vtk");
-    
-    return EXIT_SUCCESS;
-}
->>>>>>> Stashed changes
