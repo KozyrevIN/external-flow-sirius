80d23855f7bda655563a068e0265895c8e426f95
root <ivantey.kozyrev@gmail.com>
2025-08-06 20:05:32 +0300

    Clean up project files and update build configuration

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,6 @@
 #include <vtkSmartPointer.h>
 #include <vtkXMLPolyDataWriter.h>
 
-#include "../include/attach_f.h"
 #include "../include/functions.h"
 #include "../include/kernels.h"
 #include "../include/utils.h"
@@ -17,8 +16,10 @@ const std::string mesh_out_path = "meshes/sphere_642_w_fields.vtp";
 int main(int argc, char *argv[]) {
     auto mesh = load_mesh(mesh_in_path, false);
 
+    add_center_to_cells(mesh);
     attach_f(mesh, f_2);
     attach_area(mesh);
+    attach_f_true_grad(mesh, grad_f_2);
 
     auto writer = vtkSmartPointer<vtkXMLPolyDataWriter>::New();
     writer->SetFileName(mesh_out_path.c_str());
